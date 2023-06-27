8c4c5b10afab38038c3516d16cea3af1253961e1
telman03 <telmangadimov1@gmail.com>
2023-06-27 13:05:19 +0400

    Final🏁

--- src/main/java/com/az/taskmanagementsystem/model/Task.java
+++ src/main/java/com/az/taskmanagementsystem/model/Task.java
@@ -4,8 +4,6 @@ import com.az.taskmanagementsystem.model.enums.TaskStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
